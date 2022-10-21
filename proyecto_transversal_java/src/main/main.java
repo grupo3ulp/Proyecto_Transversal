dbccb49ccb6e7430631ccb71618169f8e41aa634
Marco Silva <nelsonmarcosilva@gmail.com>
2022-10-21 16:54:03 -0300

    Agregado metodo agregarAlumno AlumnoData

--- proyecto_transversal_java/src/main/main.java
+++ proyecto_transversal_java/src/main/main.java
@@ -1,9 +1,19 @@
-
 package main;
+
+import entidades.Alumno;
+import java.time.LocalDate;
+import persistencia.Conexion;
+import persistencia.DataAlumno;
 
 public class main {
 
     public static void main(String[] args) {
-       
+
+        Alumno alu1 = new Alumno("32569789", "nombre", "apellido", LocalDate.now(), true);
+
+        DataAlumno dataAlumno = new DataAlumno();
+
+        dataAlumno.guardarAlumno(alu1);
+
     }
 }
--- proyecto_transversal_java/src/persistencia/DataAlumno.java
+++ proyecto_transversal_java/src/persistencia/DataAlumno.java
@@ -1,13 +1,56 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package persistencia;
 
-/**
- *
- * @author Heisen
- */
+import entidades.Alumno;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 public class DataAlumno {
-    
+
+    private Connection conec;
+
+    public DataAlumno() {
+        this.conec = Conexion.getConexion();
+    }
+
+    public void guardarAlumno(Alumno a) {
+
+        try {
+            String sql = "INSERT INTO `alumno`(`dni`, `nombre`, `apellido`, `fecha_nacimiento`, `estado`) VALUES (?,?,?,?,?)";
+            PreparedStatement ps = conec.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, a.getDni());
+            ps.setString(2, a.getNombre());
+            ps.setString(3, a.getApellido());
+            ps.setDate(4, java.sql.Date.valueOf(a.getFecha_nacimiento()));
+            ps.setBoolean(5, true);
+        
+            if (ps.executeUpdate() > 0) {
+                 JOptionPane.showMessageDialog(null, "El Registro fue agregado correctamente");
+            } else {
+                 JOptionPane.showMessageDialog(null, "No se pudo agregar el registro");
+
+            }
+           
+            ResultSet rs = ps.getGeneratedKeys();
+
+            if (rs.next()) {
+                a.setId_alumno(rs.getInt(1));
+            }
+            
+
+            conec.close();
+        } catch (SQLException ex) {
+           JOptionPane.showMessageDialog(null, "Sentencia SQL Erronea");
+
+        }
+
+    }
+
 }
