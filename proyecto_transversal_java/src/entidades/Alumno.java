466df9e2cf05083b0e6bd618be3e13c21b792ac1
Marco Silva <nelsonmarcosilva@gmail.com>
2022-10-21 15:36:10 -0300

    agregado de clases

---
+++ proyecto_transversal_java/src/entidades/Alumno.java
@@ -0,0 +1,85 @@
+package entidades;
+
+
+import java.time.LocalDate;
+
+
+public class Alumno  {
+
+
+    private int id_alumno;
+    private String dni;
+    private String nombre;
+    private String apellido;
+    private LocalDate fecha_nacimiento;
+    private boolean estado;
+
+    public Alumno() {
+    }
+
+    public Alumno(int id_alumno, String dni, String nombre, String apellido, LocalDate fecha_nacimiento, boolean estado) {
+        this.id_alumno = id_alumno;
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fecha_nacimiento = fecha_nacimiento;
+        this.estado = estado;
+    }
+
+    public Alumno(String dni, String nombre, String apellido, LocalDate fecha_nacimiento, boolean estado) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fecha_nacimiento = fecha_nacimiento;
+        this.estado = estado;
+    }
+
+    public int getId_alumno() {
+        return id_alumno;
+    }
+
+    public void setId_alumno(int id_alumno) {
+        this.id_alumno = id_alumno;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public LocalDate getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(LocalDate fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+}
---
+++ proyecto_transversal_java/src/entidades/Inscripcion.java
@@ -0,0 +1,61 @@
+package entidades;
+
+
+public class Inscripcion  {
+
+  
+    private int id_inscripcion;
+    private int id_materia;
+    private int id_alumno;
+    private float nota;
+
+    public Inscripcion() {
+
+    }
+
+    public Inscripcion(int id_inscripcion, int id_materia, int id_alumno, float nota) {
+        this.id_inscripcion = id_inscripcion;
+        this.id_materia = id_materia;
+        this.id_alumno = id_alumno;
+        this.nota = nota;
+    }
+
+    public Inscripcion(int id_materia, int id_alumno, float nota) {
+        this.id_materia = id_materia;
+        this.id_alumno = id_alumno;
+        this.nota = nota;
+    }
+
+    public int getId_inscripcion() {
+        return id_inscripcion;
+    }
+
+    public void setId_inscripcion(int id_inscripcion) {
+        this.id_inscripcion = id_inscripcion;
+    }
+
+    public int getId_materia() {
+        return id_materia;
+    }
+
+    public void setId_materia(int id_materia) {
+        this.id_materia = id_materia;
+    }
+
+    public int getId_alumno() {
+        return id_alumno;
+    }
+
+    public void setId_alumno(int id_alumno) {
+        this.id_alumno = id_alumno;
+    }
+
+    public float getNota() {
+        return nota;
+    }
+
+    public void setNota(float nota) {
+        this.nota = nota;
+    }
+
+}
